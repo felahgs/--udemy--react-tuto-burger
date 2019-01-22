7ca66cb4ffe13c31600edd74b0655b1acde296c6
Felipe Garcia de Souza <f.souza@cinq.com.br>
2019-01-22 13:59:55 -0200

    add navigation link to the home page

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import classes from './NavigationItem.css';
 
 const navigationItem = ( props ) => (
     <ul className={classes.List}>
         <li className={classes.NavigationItem}>
-        <a 
-            href={props.link}
-            className={props.active ? classes.active: null}>{props.children}</a>
+        <NavLink
+            exact={props.exact}
+            to={props.link}
+            activeClassName={classes.active}>
+            {props.children}
+            {/* href={props.link} */}
+            {/* // className={props.active ? classes.active: null}>{props.children}</a> */}
+        </NavLink>
         </li>
     </ul>
 
