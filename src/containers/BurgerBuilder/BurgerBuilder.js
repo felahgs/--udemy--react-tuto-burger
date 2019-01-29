c82e2d31fc7e7d95131b38bba874e0ffe9205bc0
Felipe Garcia de Souza <f.souza@cinq.com.br>
2019-01-29 10:36:53 -0200

    added redux to checkout and contactData

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -43,7 +43,7 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el;
             }, 0);
-        this.setState({purchasable: sum > 0});
+       return sum > 0;
     }
 
     purchaseHandler = () => {
@@ -56,17 +56,18 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         
-        const queryParams = [];
-        for (let i in this.state.ingredients) {
-            //encondeURIComponents encodes elements to be used on URL, like whitspaces
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        }
-        queryParams.push('price=' + this.props.price);
-        const queryString = queryParams.join('&');
-        this.props.history.push({
-            pathname: '/checkout',
-            search: '?' + queryString
-        });
+        // const queryParams = [];
+        // for (let i in this.state.ingredients) {
+        //     //encondeURIComponents encodes elements to be used on URL, like whitspaces
+        //     queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        // }
+        // queryParams.push('price=' + this.props.price);
+        // const queryString = queryParams.join('&');
+        // this.props.history.push({
+        //     pathname: '/checkout',
+        //     search: '?' + queryString
+        // });
+        this.props.history.push('/checkout');
     }
 
     render () {
@@ -87,7 +88,7 @@ class BurgerBuilder extends Component {
                         ingredientAdded={this.props.onIngredientAdded}
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
                         price={this.props.price}/>
                 </Aux>
