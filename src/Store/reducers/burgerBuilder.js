f262f347d74a46da02b79ef9d4654cbcb8e2e2d0
Felipe Garcia de Souza <f.souza@cinq.com.br>
2019-02-08 10:03:46 -0200

    using updateObject on contactData; Hide SideDrawer when selecting a item

--- src/Store/reducers/burgerBuilder.js
+++ src/Store/reducers/burgerBuilder.js
@@ -1,5 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
-import { updateObject } from '../utility';
+import { updateObject } from '../../shared/utility';
 
 
 // Reducers are called by the actions and are responsible for editing 
